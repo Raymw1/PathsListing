c8e22aa8ff6ea1e5a6d37dec817a398bbc2c2ec6
Rayan <rayan.rmw@gmail.com>
2022-12-05 18:44:01 -0300

    Algorithm done

--- src/App.java
+++ src/App.java
@@ -5,33 +5,43 @@ import java.util.Vector;
 
 public class App {
 	public static void main(String[] args) {
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.print("How many locals?");
-//		Integer localsNumber = scanner.nextInt();
-		Integer localsNumber = 6;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("How many locals? ");
+		Integer localsNumber = scanner.nextInt();
 		Character character = 'A';
 		List<Character> charsVector = new ArrayList<>();
 		for (int i = 0; i < localsNumber; i++) {
 			charsVector.add(character++);
 		}
-		List<List<Character>> vectorsList = new ArrayList<>();
-		vectorsList.add(charsVector);
-		vectorsList = recursionVector(vectorsList, charsVector, localsNumber);
-		System.out.println(vectorsList);
 		
-	
-
+		List<Integer> intVector = new ArrayList<>();
+		for (int i = 0; i < localsNumber; i++) {
+			intVector.add(i);
+		}
+		
+		List<List<Character>> vectorsList = new ArrayList<>();
+		List<Character> currentVector = new ArrayList<>();
+		vectorsList = recursionVector(vectorsList, charsVector, intVector, currentVector);
+		
+		System.out.println(vectorsList);
+		System.out.println(vectorsList.size());
 	}
 	
 	
-	static List<List<Character>> recursionVector(List<List<Character>> vectorsList, List<Character> vector, Integer count) {
-		if (count == 1) {
-			return vectorsList;
+	static List<List<Character>> recursionVector(List<List<Character>> vectorsList, List<Character> charsVector, List<Integer> intVector, List<Character> currentVector) {
+		for (int i : intVector) {
+			var newCurrentVector = new ArrayList<>(currentVector);
+			newCurrentVector.add(charsVector.get(i));
+			if (intVector.size() == 1) {
+				newCurrentVector.add(newCurrentVector.get(0));
+				vectorsList.add(newCurrentVector);
+			} else {
+				var newIntVector = new ArrayList<>(intVector);
+				newIntVector.remove((Integer) i);
+				recursionVector(vectorsList, charsVector, newIntVector, newCurrentVector);	
+			}
 		}
-		var newVector = new ArrayList<>(vector);
-		newVector.add(0, newVector.get(newVector.size()-1));
-		newVector.remove(newVector.size()-1);
-		vectorsList.add(newVector);
-		return recursionVector(vectorsList, newVector, count-1);
+
+		return vectorsList;
 	}
 }
